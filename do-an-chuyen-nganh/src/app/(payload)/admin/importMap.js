c27c3a7e429475352793bc030869b16901f5ef40
nguyenhuynhphuongloc <147539137+nguyenhuynhphuongloc@users.noreply.github.com>
2026-05-04 20:17:25 +0700

    Update importMap.js

--- do-an-chuyen-nganh/src/app/(payload)/admin/importMap.js
+++ do-an-chuyen-nganh/src/app/(payload)/admin/importMap.js
@@ -1,7 +1,5 @@
-import { default as default_8a580231236cf11e308cbd8b138ada5f } from '../../../components/ForceLightTheme'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
-  "./components/ForceLightTheme#default": default_8a580231236cf11e308cbd8b138ada5f,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
